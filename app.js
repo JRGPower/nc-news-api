c5bbb58e0f661cf008737bb11c39a3888d85e4b9
JRGPower <joulespowah@gmail.com>
2022-11-14 15:05:29 +0000

    added default error handler catchAll to app and controller

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const { getTopics, invalidURL } = require('./controllers/topics.controllers')
+const { getTopics, invalidURL, catchAll } = require('./controllers/topics.controllers')
 
 const app = express()
 
 app.get('/api/topics', getTopics)
 
 app.all('/*', invalidURL)
+
+app.use(catchAll)
 
 module.exports = app
--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -11,3 +11,7 @@ exports.getTopics = (req, res, next) => {
 exports.invalidURL = (req, res,) => {
     res.status(404).send({ msg: "invalid url" })
 }
+
+exports.catchAll = (err, req, res, next) => {
+    res.status(500).send({ msg: "internal server error" });
+};
