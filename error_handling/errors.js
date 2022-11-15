adff425ef564607004f2498a87ca78ceb71f9f69
JRGPower <joulespowah@gmail.com>
2022-11-15 13:30:03 +0000

    adding error handing function for 400 Bad request

--- error_handling/errors.js
+++ error_handling/errors.js
@@ -2,6 +2,16 @@ exports.invalidURL = (req, res,) => {
     res.status(404).send({ msg: "invalid url" })
 }
 
+exports.invalidInput = (err, req, res, next) => {
+    if (err.code === "22P02") {
+        res.status(400).send({ msg: "Bad Request" });
+    } else {
+        next(err);
+    }
+};
+
 exports.catchAll = (err, req, res, next) => {
+    console.log(err)
+    //console.log(err.status, err.msg)
     res.status(500).send({ msg: "internal server error" });
 };
