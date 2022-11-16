c110e90fec2225ab10e387607428e75ba716af53
JRGPower <joulespowah@gmail.com>
2022-11-16 11:44:34 +0000

    adding error handling for SQL foreign key violation error code

--- error_handling/errors.js
+++ error_handling/errors.js
@@ -3,7 +3,7 @@ exports.invalidURL = (req, res,) => {
 }
 
 exports.invalidInput = (err, req, res, next) => {
-    if (err.code === "22P02") {
+    if (err.code === "22P02" || err.code == "23503") {
         res.status(400).send({ msg: "Bad Request" });
     } else {
         next(err);
@@ -14,5 +14,6 @@ exports.catchAll = (err, req, res, next) => {
     if (err.status && err.msg) {
         res.status(err.status).send({ msg: err.msg })
     }
+    console.log(err);
     res.status(500).send({ msg: "internal server error" });
 };
