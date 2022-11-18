5aec824b3f8b3023becbbb1fa6a4a33893d05bd4
JRGPower <joulespowah@gmail.com>
2022-11-18 10:35:12 +0000

    changing tests to be less brittle

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -1,9 +1,10 @@
 const { readFile } = require('fs/promises')
 
-exports.getApi = (res, req, next) => {
-    return readFile(`${__dirname}/db/endpoints.json`, "utf8").then((data) => {
-        res.send(JSON.parse(data))
-    }).catch((err => {
-        next(err)
-    }))
+exports.getApi = (req, res, next) => {
+    return readFile(`${__dirname}/../db/endpoints.json`, "utf8")
+        .then((data) => {
+            res.send(JSON.parse(data))
+        }).catch((err => {
+            next(err)
+        }))
 }
