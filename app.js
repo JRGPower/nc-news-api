65e887688a088e32e97f2a0b025b6d06b5c9969d
JRGPower <joulespowah@gmail.com>
2022-12-05 10:53:49 +0000

    allow cross orgin resource sharing

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
-const express = require('express')
-const { getArticles, getArticleById, getArticleComments, postArticleComment, patchArticle } = require('./controllers/articles.controllers')
-const { deleteComment } = require('./controllers/comments.controllers')
-const { getTopics } = require('./controllers/topics.controllers')
-const { getUsers } = require('./controllers/users.controllers')
-const { invalidURL, catchAll, invalidInput, } = require('./error_handling/errors')
-const { getApi } = require('./controllers/api.controllers')
+const express = require("express");
+const cors = require("cors");
+const {
+  getArticles,
+  getArticleById,
+  getArticleComments,
+  postArticleComment,
+  patchArticle,
+} = require("./controllers/articles.controllers");
+const { deleteComment } = require("./controllers/comments.controllers");
+const { getTopics } = require("./controllers/topics.controllers");
+const { getUsers } = require("./controllers/users.controllers");
+const {
+  invalidURL,
+  catchAll,
+  invalidInput,
+} = require("./error_handling/errors");
+const { getApi } = require("./controllers/api.controllers");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/api', getApi)
+app.get("/api", getApi);
 
-app.get('/api/topics', getTopics)
+app.get("/api/topics", getTopics);
 
-app.get("/api/users", getUsers)
+app.get("/api/users", getUsers);
 
-app.delete("/api/comments/:comment_id", deleteComment)
+app.delete("/api/comments/:comment_id", deleteComment);
 
-app.get('/api/articles', getArticles)
-app.get('/api/articles/:article_id', getArticleById)
-app.get('/api/articles/:article_id/comments', getArticleComments)
+app.get("/api/articles", getArticles);
+app.get("/api/articles/:article_id", getArticleById);
+app.get("/api/articles/:article_id/comments", getArticleComments);
 
-app.post('/api/articles/:article_id/comments', postArticleComment)
+app.post("/api/articles/:article_id/comments", postArticleComment);
 
-app.patch('/api/articles/:article_id', patchArticle)
+app.patch("/api/articles/:article_id", patchArticle);
 
-app.all('/*', invalidURL)
+app.all("/*", invalidURL);
 
-app.use(invalidInput)
-app.use(catchAll)
+app.use(invalidInput);
+app.use(catchAll);
 
-module.exports = app
-
+module.exports = app;
